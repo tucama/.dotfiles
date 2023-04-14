672c67dfb7f8d71ee581429a7ec8c9afa8cd47c1
Tucamar <xxtucamarxx@hotmail.com>
2023-04-14 15:10:19 -0300

    alacritty

--- bash/.bashrc
+++ bash/.bashrc
@@ -117,3 +117,4 @@ if ! shopt -oq posix; then
   fi
 fi
 
+. "$HOME/.cargo/env"
--- nvim/.config/nvim/lua/user/colorscheme.lua
+++ nvim/.config/nvim/lua/user/colorscheme.lua
@@ -8,6 +8,5 @@ try
   colorscheme gruvbox
 catch /^Vim\%((\a\+)\)\=:E185/
   colorscheme default
-  set background=dark
 endtry
 ]])
