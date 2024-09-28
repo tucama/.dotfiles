51023de35c8007060619cfef8649088e64d141bf
Tucamar <xxtucamarxx@hotmail.com>
2024-09-28 09:18:01 -0300

    remove ~ after EOL

--- nvim/.config/nvim/lua/tucamar/core/options.lua
+++ nvim/.config/nvim/lua/tucamar/core/options.lua
@@ -1,53 +1,55 @@
 local options = {
-	foldenable = false,
-	backup = false, -- creates a backup file
-	clipboard = "unnamedplus", -- allows neovim to access the system clipboard
-	cmdheight = 1, -- more space in the neovim command line for displaying messages
-	completeopt = { "menuone", "noselect" }, -- mostly just for cmp
-	conceallevel = 0, -- so that `` is visible in markdown files
-	fileencoding = "utf-8", -- the encoding written to a file
-	hlsearch = false, -- highlight all matches on previous search pattern
-	incsearch = true,
-	ignorecase = true, -- ignore case in search patterns
-	mouse = "a", -- allow the mouse to be used in neovim
-	pumheight = 7, -- pop up menu height
-	showmode = false, -- show insert/normal/visual mode
-	smartcase = true, -- smart case
-	smartindent = true, -- make indenting smarter again
-	splitbelow = true, -- force all horizontal splits to go below current window
-	splitright = true, -- force all vertical splits to go to the right of current window
-	swapfile = false, -- creates a swapfile
-	termguicolors = true, -- set term gui colors (most terminals support this)
-	timeoutlen = 500, -- time to wait for a mapped sequence to complete (in milliseconds)
-	undofile = true, -- enable persistent undo
-	updatetime = 300, -- faster completion (4000ms default)
-	writebackup = false, -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
-	expandtab = true, -- convert tabs to spaces
-	softtabstop = 4,
-	tabstop = 4, -- insert 2 spaces for a tab
-	shiftwidth = 4, -- the number of spaces inserted for each indentation
-	cursorline = true, -- highlight the current line
-	nu = true, -- set numbered lines
-	relativenumber = true, -- set relative numbered lines
-	numberwidth = 4, -- set number column width to 4
-	signcolumn = "auto", -- always show the sign column, otherwise it would shift the text each time
-	wrap = false, -- display lines as one long line
-	scrolloff = 10, -- is one of my fav
-	sidescrolloff = 30,
-	guifont = "monospace:h17", -- the font used in graphical neovim applications
+    foldenable = false,
+    fillchars = { eob = " " },
+    backup = false, -- creates a backup file
+    clipboard = "unnamedplus", -- allows neovim to access the system clipboard
+    cmdheight = 1, -- more space in the neovim command line for displaying messages
+    completeopt = { "menuone", "noselect" }, -- mostly just for cmp
+    conceallevel = 0, -- so that `` is visible in markdown files
+    fileencoding = "utf-8", -- the encoding written to a file
+    hlsearch = false, -- highlight all matches on previous search pattern
+    incsearch = true,
+    ignorecase = true, -- ignore case in search patterns
+    mouse = "a", -- allow the mouse to be used in neovim
+    pumheight = 7, -- pop up menu height
+    showmode = false, -- show insert/normal/visual mode
+    smartcase = true, -- smart case
+    smartindent = true, -- make indenting smarter again
+    splitbelow = true, -- force all horizontal splits to go below current window
+    splitright = true, -- force all vertical splits to go to the right of current window
+    swapfile = false, -- creates a swapfile
+    termguicolors = true, -- set term gui colors (most terminals support this)
+    timeoutlen = 500, -- time to wait for a mapped sequence to complete (in milliseconds)
+    undofile = true, -- enable persistent undo
+    updatetime = 300, -- faster completion (4000ms default)
+    writebackup = false, -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+    expandtab = true, -- convert tabs to spaces
+    softtabstop = 4,
+    tabstop = 4, -- insert 2 spaces for a tab
+    shiftwidth = 4, -- the number of spaces inserted for each indentation
+    cursorline = true, -- highlight the current line
+    nu = true, -- set numbered lines
+    relativenumber = true, -- set relative numbered lines
+    numberwidth = 4, -- set number column width to 4
+    signcolumn = "auto", -- always show the sign column, otherwise it would shift the text each time
+    wrap = false, -- display lines as one long line
+    scrolloff = 10, -- is one of my fav
+    laststatus = 2,
+    sidescrolloff = 30,
+    guifont = "monospace:h17", -- the font used in graphical neovim applications
 }
 
 vim.api.nvim_create_autocmd("ColorScheme", {
-	group = vim.api.nvim_create_augroup("QuickScopeHighlights", { clear = true }),
-	desc = "Quickscope highlight",
-	pattern = "*",
-	callback = function()
-		vim.g.qs_highlight_on_keys = { "f", "F", "t", "T" }
-		vim.api.nvim_command("highlight QuickScopePrimary guifg=#afff5f gui=underline ctermfg=155 cterm=underline")
-		vim.api.nvim_command("highlight QuickScopeSecondary guifg=#5fffff gui=underline ctermfg=81 cterm=underline")
-	end,
+    group = vim.api.nvim_create_augroup("QuickScopeHighlights", { clear = true }),
+    desc = "Quickscope highlight",
+    pattern = "*",
+    callback = function()
+        vim.g.qs_highlight_on_keys = { "f", "F", "t", "T" }
+        vim.api.nvim_command("highlight QuickScopePrimary guifg=#afff5f gui=underline ctermfg=155 cterm=underline")
+        vim.api.nvim_command("highlight QuickScopeSecondary guifg=#5fffff gui=underline ctermfg=81 cterm=underline")
+    end,
 })
 
 for k, v in pairs(options) do
-	vim.opt[k] = v
+    vim.opt[k] = v
 end
