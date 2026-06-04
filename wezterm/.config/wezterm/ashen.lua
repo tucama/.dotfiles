b092aeb5b7dde04eac90d0b5e82c5bc19b22d000
tucama <xxtucamarxx@gmail.com>
2026-06-04 21:04:45 +0200

    wezterm ashen colorscheme

--- wezterm/.config/wezterm/ashen.lua
+++ wezterm/.config/wezterm/ashen.lua
@@ -3,15 +3,36 @@
 -- License: MIT
 --
 return {
-    foreground = "#f5f5f5",
-    background = "#121212",
+    foreground = 
+        "#f5f5f5",
+    background = 
+        "#121212",
 
-    cursor_bg = "#f5f5f5",
-    cursor_fg = "#121212",
-    cursor_border = "#f5f5f5",
+    cursor_bg = 
+        "#f5f5f5",
+    cursor_fg = 
+        "#121212",
+    cursor_border = 
+        "#f5f5f5",
 
-    selection_fg = "#f5f5f5",
-    selection_bg = "#1d1d1d",
+    selection_fg = 
+        -- "#121212",
+        -- "#B14242",
+        -- "#D87C4A",
+        "#E49A44",
+        -- "#4A8B8B",
+        -- "#a7a7a7",
+        -- "#b4b4b4",
+        -- "#d5d5d5",
+    selection_bg = 
+        -- "#121212",
+        "#B14242",
+        -- "#D87C4A",
+        -- "#E49A44",
+        -- "#4A8B8B",
+        -- "#a7a7a7",
+        -- "#b4b4b4",
+        -- "#d5d5d5",
 
     scrollbar_thumb = "#121212",
     split = "#121212",
